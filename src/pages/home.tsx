bad167a588c51bb011676de0d1241f15039ad206
Will Demaine <willdemaine@gmail.com>
2021-12-16 16:38:25 +0000

    wip implement design

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,116 +1,166 @@
-import { Link } from "react-router-dom";
-import gmbg from "./images/gm2.jpeg";
-import gmdrop from "./images/gm3.jpeg";
+import Header from "../components/layout/header";
+
+import tokenImg from "./images/gm1.jpeg";
+import boxImg from "./images/box.png";
+import heroimg from "./images/fjall.png";
+
+const bgImageUrl = `url('${heroimg}')`;
 
 const Home = () => {
   return (
-    <>
-      <div className="relative bg-black overflow-hidden text-gray-50">
-        <div className="max-w-7xl mx-auto">
-          <div className="relative z-10 pb-8 bg-black sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-            <svg
-              className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-black transform translate-x-1/2"
-              fill="currentColor"
-              viewBox="0 0 100 100"
-              preserveAspectRatio="none"
-              aria-hidden="true"
-            >
-              <polygon points="50,0 100,0 50,100 0,100" />
-            </svg>
-
-            <div className="relative pt-6 px-4 sm:px-6 lg:px-8 hidden md:inline-block">
-              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div className="flex items-center justify-between w-full md:w-auto"></div>
-                </div>
-                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8"></div>
-              </nav>
-            </div>
-
-            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-10 lg:px-8 xl:mt-16">
-              <div className="sm:text-center lg:text-left">
-                <h1 className="text-4xl tracking-tight font-extrabold text-gray-50 sm:text-5xl md:text-4xl">
-                  <span className="block xl:inline ">The gm club is a</span>{" "}
-                  <span className="block grad-primary grad-primary-text xl:inline">
-                    Decentralised Autonomous Organisation
-                  </span>
-                </h1>
-                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  We are a collective of NFT enthusiasts, artists, programmers, collectors and crypto-natives.
+    <div>
+      <div className="bg-cover bg-no-repeat drop-shadow-md" style={{ backgroundImage: bgImageUrl }}>
+        <Header />
+        <div className="max-w-7xl h-full mx-auto pb-20 sm:px-6 lg:px-8 text-center">
+          <div className="text-gray-50 h-4/5 mt-12">
+            <div className="max-h-[128px] md:max-h-[256px] lg:max-h-[512px] flex justify-center relative">
+              <div className="absolute top-[32px] md:top-[64px] lg:top-[128px] flex">
+                <p className="text-[3rem] leading-[3rem] md:text-[6rem] md:leading-[6rem] lg:text-[12rem] lg:leading-[12rem] font-semibold text-shadow-md font-serif align-middle">
+                  gm.
                 </p>
-                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                  <div className="mt-3 sm:mt-0">
-                    <a
-                      href="https://studio.gmdao.ai"
-                      className="w-full flex items-center justify-center py-3 border border-transparent text-base font-medium rounded-md text-white bg-[#8d0ffa] hover:bg-[#8c0ffa8f] md:py-4 md:text-lg md:px-10"
-                    >
-                      See our collections
-                    </a>
-                  </div>
-                </div>
               </div>
-            </main>
+              <img className=" object-contain" src={boxImg} />
+            </div>
+            <p className="mt-8">A Decentralised Autonomous Organization</p>
           </div>
         </div>
-        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src={gmbg} alt="" />
-        </div>
       </div>
-      <div className="mt-24">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+      <div id="about" className="max-w-6xl mx-auto py-6 pt-12 text-gray-800">
+        <h1 className="text-2xl pb-6 font-semibold">
+          Supporting established and upcoming artists by providing a launchpad for their work
+        </h1>
+        <div className="space-y-4">
+          <p className="text-lg">
+            With our platform we provide an end-to-end service where everything is managed, from smart contract
+            deployment, marketing & revenue generation, as an out of the box solution.
+          </p>
+          <p>
+            Each body of work will be treated as unique scope of work & we strike to deliver an completely tailored
+            experience to match you vision, whether that be 1/1’s or collection of 10,000 pieces. Occaecati sit fugiat
+            qui mollitia. Iste odio consequatur perspiciatis eos. Voluptatum repudiandae dolorum similique id eaque
+            voluptatum totam praesentium. Molestiae laboriosam sed repudiandae aperiam maiores minima accusantium illo
+            sint. Et sed et velit est ea molestias corrupti. Sint nesciunt architecto libero deserunt eaque.
+          </p>
+
+          <p>
+            Dolorum distinctio alias voluptatibus fugit. Voluptas accusamus sapiente quas voluptatum magni sit numquam
+            quisquam. Excepturi officiis cupiditate error id temporibus at quis. Consectetur labore necessitatibus atque
+            sequi nulla odio corporis voluptatem ab. Cum accusantium natus est incidunt exercitationem dignissimos sunt.
+            Nemo excepturi quo.
+          </p>
+
+          <p>
+            A aut laborum non laudantium voluptas et est aliquam. Vero numquam harum aperiam blanditiis perspiciatis
+            eveniet debitis blanditiis officiis. Optio dolore qui earum sapiente qui asperiores pariatur. Quia impedit
+            aperiam accusamus occaecati voluptates provident molestias. Corrupti voluptatem praesentium qui quae
+            suscipit maiores quas necessitatibus. Officiis cum soluta et nihil nemo laudantium.
+          </p>
+        </div>
+        <div className="my-8">
+          <BrandButton text="Apply for the studio" />
+        </div>
+        <Divider text={"gm"} />
+        <h1 id="token" className="text-2xl pb-6 font-semibold">
+          The token
+        </h1>
+        <div className="space-y-4">
+          <div className="grid grid-cols-2 gap-8">
             <div>
-              <div className="mt-6">
-                <h2 className="text-3xl font-extrabold tracking-tight text-gray-50">See our latest drop</h2>
-                <p className="mt-4 text-lg text-gray-500">
-                  Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
-                  porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus
-                  viverra dui tellus ornare pharetra.
-                </p>
-                <div className="mt-6">
-                  <a
-                    href="https://studio.gmdao.ai"
-                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-[#8d0ffa] hover:bg-[#8c0ffa8f]"
-                  >
-                    Visit Studio
-                  </a>
-                </div>
+              <p className="text-lg">
+                To build this community, we created a token. Currently built upon the Rarible platform, and soon to be
+                migrated to our own propritary contract, the token will act as your way to gain entry. Beyond that, it
+                also give each holder the right to vote on how the comunity evolves & spends funds from the gmDAO.
+                Officia id nisi temporibus hic. Voluptatem soluta occaecati tenetur maxime nihil alias deleniti. Aperiam
+                eos repudiandae consequatur libero officiis deleniti. Autem ipsa soluta iure et.
+              </p>
+              <div className="my-8 flex space-x-4">
+                <BrandButton text="Get a token" />
+                <BrandButton text="Migrate to V2" />
               </div>
             </div>
-          </div>
-          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              <img
-                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                src={gmdrop}
-                alt="gm studio"
-              />
+            <div className="w-full">
+              <img src={tokenImg} />
             </div>
           </div>
         </div>
-      </div>
-      <div id="about" className="mt-20 flex flex-col space-y-4">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-50 mb-2">About the dao</h2>
+        <Divider text={"ge"} />
+        <h1 className="text-2xl pb-6 font-semibold">The team</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id velit rhoncus, euismod neque et, aliquet
-          elit. Etiam risus urna, molestie eu eros id, volutpat porta mi. Mauris lobortis tortor non lacus lobortis, non
-          congue turpis maximus. Morbi interdum neque pulvinar massa malesuada porta. Pellentesque fermentum accumsan
-          imperdiet. Phasellus ac malesuada mauris. Aenean tellus odio, maximus eget orci eu, hendrerit pretium dolor.
-          Suspendisse accumsan viverra sapien. Suspendisse potenti. Sed viverra urna at tincidunt facilisis. Ut
-          condimentum scelerisque nunc, et malesuada diam. Fusce eget arcu mattis, accumsan enim et, malesuada libero.
-          Morbi rhoncus ut ante vel tincidunt.
+          Coalescing from across various timezones the gm leadership works to curate experiences and accessibility
+          throughout the platform. Comprised of various backrounds throughout technology and artistic circles we
+          Voluptatem soluta occaecati tenetur maxime nihil alias deleniti. Aperiam eos repudiandae consequatur libero
+          officiis deleniti. Autem ipsa soluta iure et.
         </p>
-        <p>
-          Aliquam erat volutpat. Vestibulum varius feugiat sapien, at tempus ligula imperdiet sit amet. Donec egestas
-          justo lacus, ac accumsan orci consequat ut. Nulla condimentum massa ut tristique pellentesque. Donec vitae
-          neque eu eros aliquet porttitor. Sed eleifend quis tortor vel blandit. Ut porta molestie augue at viverra.
-          Donec rhoncus nibh risus, quis posuere justo faucibus ac. Nam venenatis orci a mi placerat ullamcorper.
-          Suspendisse laoreet a velit ac mattis. Praesent varius quam risus, non porta enim efficitur id. Aliquam erat
-          volutpat. Fusce mi dolor, elementum id lorem a, convallis suscipit nulla. Nulla leo risus, auctor ut sagittis
-          sit amet, ultricies eu nunc. Etiam fringilla ligula nec molestie efficitur.
-        </p>
+        <div className="grid grid-cols-4 gap-8 my-8">
+          <Profile name="Cyphr" title="Benevolent Dictator" image={tokenImg}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem saepe, nesciunt veniam culpa omnis
+            praesentium molestias nemo sit. Odio praesentium atque impedit asperiores pariatur commodi a in expedita
+            dolore.
+          </Profile>
+          <Profile name="Cyphr" title="Benevolent Dictator" image={tokenImg}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem saepe, nesciunt veniam culpa omnis
+            praesentium molestias nemo sit. Odio praesentium atque impedit asperiores pariatur commodi a in expedita
+            dolore.
+          </Profile>
+          <Profile name="Cyphr" title="Benevolent Dictator" image={tokenImg}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem saepe, nesciunt veniam culpa omnis
+            praesentium molestias nemo sit. Odio praesentium atque impedit asperiores pariatur commodi a in expedita
+            dolore.
+          </Profile>
+          <Profile name="Cyphr" title="Benevolent Dictator" image={tokenImg}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem saepe, nesciunt veniam culpa omnis
+            praesentium molestias nemo sit. Odio praesentium atque impedit asperiores pariatur commodi a in expedita
+            dolore.
+          </Profile>
+        </div>
+        <Divider text={"gn"} />
       </div>
-    </>
+    </div>
+  );
+};
+
+const Divider = ({ text }: { text: string }) => {
+  return (
+    <div className="relative my-16">
+      <div className="absolute inset-0 flex items-center" aria-hidden="true">
+        <div className="w-full border-t border-gray-300" />
+      </div>
+      <div className="relative flex justify-center">
+        <span className="px-2 bg-white text-sm text-gray-500">{text}</span>
+      </div>
+    </div>
+  );
+};
+
+const BrandButton = ({ text }: { text: string }) => {
+  return (
+    <button className="bg-gray-800 hover:bg-gray-900 text-white py-3 px-4 rounded-sm focus:outline-none focus:shadow-outline">
+      {text}
+    </button>
+  );
+};
+
+type ProfileProps = {
+  name: string;
+  title: string;
+  image: string;
+  children: React.ReactNode;
+};
+
+const Profile = (props: ProfileProps) => {
+  return (
+    <div className="flex flex-col space-y-4">
+      <div className="flex flex-col space-y-2">
+        <div className="flex items-center">
+          <img className="w-12 h-12 rounded-full" src={props.image} />
+          <div className="flex-1 ml-4">
+            <h1 className="text-lg font-semibold">{props.name}</h1>
+            <p className="text-sm">{props.title}</p>
+          </div>
+        </div>
+        <p className="text-sm">{props.children}</p>
+      </div>
+    </div>
   );
 };
 
