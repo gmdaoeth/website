b131192659264373be1ca1bf232aafe6dc92362e
Tyche Collective <tychecollective@protonmail.com>
2022-01-02 19:10:41 +0000

    Update deploy script

--- scripts/hardhatDeploy.js
+++ scripts/hardhatDeploy.js
@@ -1,13 +1,34 @@
+const gmDaoMetadataURI = "https://gmdao.ai/metadata/";
+const raribleContractAddress = "0xd07dc4262BCDbf85190C01c996b4C06a461d2430";
+
 async function main() {
-  const GmV2Contract = await ethers.getContractFactory("GmV2");
-  const gmv2 = await GmV2Contract.deploy("https://CHANGE_ME/");
+	const [deployer] = await ethers.getSigners();
 
-  console.log("DEPLOYED TO: ", gmv2.address);
-}
+	console.log("Deploying contracts with the account:", deployer.address);
+  
+	console.log(
+	  "Account balance: ",
+	  ethers.utils.formatEther(await deployer.getBalance())
+	);
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+	// We get the contract to deploy
+	const GmV2Contract = await ethers.getContractFactory('GmV2');
+	const gmv2 = await GmV2Contract.deploy(
+		gmDaoMetadataURI, // baseURI
+		raribleContractAddress // raribleAddress
+	);
+  
+	console.log('DEPLOYED TO: ', gmv2.address);
+
+	console.log(
+		"Account balance after deploy:",
+		ethers.utils.formatEther(await deployer.getBalance())
+	  );
+  }
+  
+  main()
+	.then(() => process.exit(0))
+	.catch(error => {
+	  console.error(error);
+	  process.exit(1);
+	});
