bb0ffc7597576bcccce060f9c1c7e24319adca35
Tyche Collective <tychecollective@protonmail.com>
2022-01-02 19:06:23 +0000

    Install packages and update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,7 +22,19 @@ module.exports = {
       },
     },
   },
+  networks: {
+     ropsten: {
+       url: process.env.ROPSTEN_ENDPOINT,
+       accounts: [process.env.ROPSTEN_PRIVATE_KEY],
+     },
+     hardhat: {
+       gasPrice: 50000000000 // in wei; in gwei it's the 1st two digits
+     }
+   },
   mocha: {
     timeout: 200000,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
