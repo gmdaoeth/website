188f0fae242d3311ad9c8d862e5baf7629a00548
Tyche Collective <tychecollective@protonmail.com>
2022-01-02 20:46:07 +0000

    Update tests

--- test/gmv2-test.js
+++ test/gmv2-test.js
@@ -12,9 +12,9 @@ let wallet1;
 let wallet2;
 let wallet3;
 let wallet4;
-let maxSupply = 899;
-let maxNormalTokens = 836;
-let maxSpecialTokens = 63;
+let maxSupply = 900;
+let maxNormalTokens = 870;
+let maxSpecialTokens = 30;
 let raribleTokenId = 706480;
 let gmdaoaddress = '0xD18e205b41eEe3D208D3B10445DB95Ff02ba4acA';
 let baseTokenURI = "https://gmdao.ai/metadata/"
@@ -30,9 +30,9 @@ async function deployAndSetup() {
   );
   [owner, wallet1, wallet2, wallet3, wallet4] =
     await hre.ethers.getSigners();
-    fakeRaribleContract = await GmV1MockContract.deploy();
+    fakeRaribleContract = await GmV1MockContract.deploy(wallet1.address);
     fakeRaribleContractAddress = fakeRaribleContract.address;
-    fakeERC1155Contract = await GmV1MockContract.deploy();
+    fakeERC1155Contract = await GmV1MockContract.deploy(wallet1.address);
   contract = await GmV2Contract.deploy(
     baseTokenURI,
     fakeRaribleContractAddress
@@ -45,67 +45,16 @@ describe("GM v2", function () {
       await deployAndSetup();
     });
 
-    it("gives deployer wallet 10 of each token", async function () {
+    it("gives deployer wallet 100 of gm token", async function () {
       let balanceOfRaribleToken = await fakeRaribleContract.balanceOf(owner.address, 1);
       let balanceOfGmToken = await fakeRaribleContract.balanceOf(owner.address, 706480);
       expect(balanceOfRaribleToken.toNumber()).to.equal(10);
-      expect(balanceOfGmToken.toNumber()).to.equal(10);
+      expect(balanceOfGmToken.toNumber()).to.equal(100);
     });
 
-    it("gives another wallet 0 of gm token", async function () {
+    it("gives another wallet 100 of gm token", async function () {
       let balanceOfGmToken = await fakeRaribleContract.balanceOf(wallet1.address, 706480);
-      expect(balanceOfGmToken.toNumber()).to.equal(0);
-    });
-  });
-
-  describe("Gas usage statistics", function () {
-    beforeEach(async function () {
-      await deployAndSetup();
-    });
-
-    it("Should deploy with less than 0.1 ETH/~$180", async () => {
-      const tx = contract.deployTransaction;
-      const receipt = await tx.wait();
-      const gasUsed = receipt.gasUsed;
-      const gasPrice = tx.gasPrice; // in wei
-      const gasCostInEth =
-        (gasPrice.toNumber() * gasUsed.toNumber()) / 1000000000000000000;
-      // gas used: 2555015
-      // cost if gas price is 50 gwei
-      expect(gasCostInEth).to.be.lessThan(0.15); 
-    });
-
-    it("Should estimate gas for transferring ERC-1155", async function () {
-      await contract.toggleMigrationActive();
-      const tx = await fakeRaribleContract.safeTransferFrom(owner.address, contract.address, raribleTokenId, 1, "0x00");
-      const receipt = await tx.wait();
-      const gasUsed = receipt.gasUsed;
-      expect(gasUsed.toNumber()).to.be.lessThanOrEqual(91583); 
-    });
-
-    it("Should estimate gas for calling upgradeToken", async function () {
-      await contract.toggleMigrationActive();
-      await fakeRaribleContract.safeTransferFrom(owner.address, contract.address, raribleTokenId, 1, "0x00");
-      const tx = await contract.upgradeToken();
-      const receipt = await tx.wait();
-      const gasUsed = receipt.gasUsed;
-      expect(gasUsed.toNumber()).to.be.lessThanOrEqual(104507);
-    });
-
-    it("Should estimate gas for calling upgradeBatch qty 5", async function () {
-      await contract.toggleMigrationActive();
-      const tx = await contract.upgradeBatch(5);
-      const receipt = await tx.wait();
-      const gasUsed = receipt.gasUsed;
-      expect(gasUsed.toNumber()).to.be.lessThanOrEqual(206742);
-    });
-
-    it("Should estimate gas for calling upgradeSpecialToken", async function () {
-      await contract.toggleMigrationActive();
-      const tx = await contract.upgradeSpecialToken();
-      const receipt = await tx.wait();
-      const gasUsed = receipt.gasUsed;
-      expect(gasUsed.toNumber()).to.be.lessThanOrEqual(85272);
+      expect(balanceOfGmToken.toNumber()).to.equal(100);
     });
   });
 
@@ -113,6 +62,18 @@ describe("GM v2", function () {
     beforeEach(async function () {
       await deployAndSetup();
     });
+
+    it("Should deploy with less than 0.132 ETH", async () => {
+	  const tx = contract.deployTransaction;
+	  const receipt = await tx.wait();
+	  const gasUsed = receipt.gasUsed;
+	  const gasPrice = tx.gasPrice; // in wei
+	  const gasCostInEth =
+		(gasPrice.toNumber() * gasUsed.toNumber()) / 1000000000000000000;
+	  // gas used: 2555015
+	  // cost if gas price is 50 gwei = 0.1310458
+	  expect(gasCostInEth).to.be.lessThan(0.132); 
+	});
 
     it("has name and symbol", async function () {
       expect(await contract.name()).to.equal("gmDAO Token v2");
@@ -124,15 +85,15 @@ describe("GM v2", function () {
       expect(number.toNumber()).to.equal(maxSupply);
     });
 
-	  it("has correct normal token supply", async function () {
+	it("has correct normal token supply", async function () {
       const number = await contract.maxNormalTokens();
       expect(number.toNumber()).to.equal(maxNormalTokens);
-	  });
+	});
 
-	  it("has correct special token supply", async function () {
+	it("has correct special token supply", async function () {
       const number = await contract.maxSpecialTokens();
       expect(number.toNumber()).to.equal(maxSpecialTokens);
-	  });
+	});
 
     it("is inactive for migration on deploy", async function () {
       const isActive = await contract.isMigrationActive();
@@ -182,6 +143,12 @@ describe("GM v2", function () {
       const tokens = await contract.getTotalTokenCount();
       expect(tokens.toNumber()).to.equal(1);
     });
+
+	it("can call upgradeSpecialBatch", async function () {
+	  await contract.upgradeSpecialBatch(5);
+	  const tokens = await contract.getTotalTokenCount();
+	  expect(tokens.toNumber()).to.equal(5);
+	});
   });
 
   describe("Admin functions", function () {
@@ -206,6 +173,15 @@ describe("GM v2", function () {
       const specialTokens = await contract.getSpecialTokenCount();
       expect(specialTokens.toNumber()).to.equal(1);
     });
+
+	it("allows only owner to call upgradeSpecialBatch", async function () {
+	  await expect(contract.connect(wallet1).upgradeSpecialBatch(5)).to.be.revertedWith(
+		"Ownable: caller is not the owner"
+	  );
+	  await contract.upgradeSpecialBatch(5);
+	  const tokens = await contract.getTotalTokenCount();
+	  expect(tokens.toNumber()).to.equal(5);
+	});
 
     it("allows only owner to transfer ownership", async function () {
       await expect(contract.connect(wallet1).transferOwnership(wallet1.address)).to.be.revertedWith(
@@ -368,11 +344,11 @@ describe("GM v2", function () {
       expect(normaltokens.toNumber()).to.equal(3);
       expect(specialtokens.toNumber()).to.equal(0);
       expect(totalTokens.toNumber()).to.equal(3);
-      await contract.upgradeBatch(833);
+      await contract.upgradeBatch(maxNormalTokens - 3);
       const newNormaltokens = await contract.getNormalTokenCount();
       const newTotalTokens = await contract.getTotalTokenCount();
-      expect(newNormaltokens.toNumber()).to.equal(836);
-      expect(newTotalTokens.toNumber()).to.equal(836);
+      expect(newNormaltokens.toNumber()).to.equal(maxNormalTokens);
+      expect(newTotalTokens.toNumber()).to.equal(maxNormalTokens);
       await expect(contract.connect(wallet4).upgradeToken()).to.be.revertedWith(
         "MAX_NORMAL_SUPPLY"
       );
@@ -382,31 +358,25 @@ describe("GM v2", function () {
     });
     
     it("doesn't allow more than the max amount of special tokens to be minted", async function () {
-      for (let i = 0; i < 10; i++) {
-        await contract.upgradeSpecialToken();
-      }
+      await contract.upgradeSpecialBatch(10);
       const normaltokens = await contract.getNormalTokenCount();
       const specialtokens = await contract.getSpecialTokenCount();
       const totalTokens = await contract.getTotalTokenCount();
       expect(normaltokens.toNumber()).to.equal(0);
       expect(specialtokens.toNumber()).to.equal(10);
       expect(totalTokens.toNumber()).to.equal(10);
-      for (let i = 0; i < 53; i++) {
-        await contract.upgradeSpecialToken();
-      }
+	  await contract.upgradeSpecialBatch(maxSpecialTokens - 10);
       const newSpecialtokens = await contract.getSpecialTokenCount();
       const newTotalTokens = await contract.getTotalTokenCount();
-      expect(newSpecialtokens.toNumber()).to.equal(63);
-      expect(newTotalTokens.toNumber()).to.equal(63);
+      expect(newSpecialtokens.toNumber()).to.equal(maxSpecialTokens);
+      expect(newTotalTokens.toNumber()).to.equal(maxSpecialTokens);
       await expect(contract.upgradeSpecialToken()).to.be.revertedWith(
         "MAX_SPECIAL_SUPPLY"
       );
     });
     
     it("doesn't allow more than the max supply of tokens to be minted", async function () {
-      for (let i = 0; i < maxSpecialTokens; i++) {
-        await contract.upgradeSpecialToken();
-      }
+	  await contract.upgradeSpecialBatch(maxSpecialTokens);
       await contract.upgradeBatch(maxNormalTokens - 1);
       const normaltokens = await contract.getNormalTokenCount();
       const specialtokens = await contract.getSpecialTokenCount();
@@ -419,14 +389,6 @@ describe("GM v2", function () {
         "MAX_TOTAL_SUPPLY"
       );
     });
-    
-    // it("", async function () {});
-    
-    // it("", async function () {});
-    
-    // it("", async function () {});
-    
-    // it("", async function () {});
   });
 
   describe("Tokens", function () {
@@ -443,10 +405,8 @@ describe("GM v2", function () {
     it("keeps track of token ids correctly", async function () {
       // mint 10 normal and 5 special
       await contract.upgradeBatch(10);
-      for (let i = 0; i < 5; i++) {
-        await contract.upgradeSpecialToken();
-      }
-      // expect total to be 15, next normal token id to be 10, next special token id to be 841
+	  await contract.upgradeSpecialBatch(5);
+      // expect total to be 15, next normal token id to be 10, next special token id to be 875
       const totalTokens = await contract.getTotalTokenCount();
       expect(totalTokens).to.equal(15);
 
@@ -458,13 +418,11 @@ describe("GM v2", function () {
       const specialTx = await contract.upgradeSpecialToken();
       const specialReceipt = await specialTx.wait();
       const createdSpecialTokenId = specialReceipt.events[0].args.tokenId;
-      expect(createdSpecialTokenId).to.equal(841);
+      expect(createdSpecialTokenId).to.equal(875);
 
       // mint 35 normal and 6 special
       await contract.upgradeBatch(35);
-      for (let i = 0; i < 6; i++) {
-        await contract.upgradeSpecialToken();
-      }
+      await contract.upgradeSpecialBatch(6);
       // expect total to be 58, next normal token id to be 46, next special token id to be 847
       const newTotalTokens = await contract.getTotalTokenCount();
       expect(newTotalTokens).to.equal(58);
@@ -477,7 +435,7 @@ describe("GM v2", function () {
       const newSpecialTx = await contract.upgradeSpecialToken();
       const newSpecialReceipt = await newSpecialTx.wait();
       const newCreatedSpecialTokenId = newSpecialReceipt.events[0].args.tokenId;
-      expect(newCreatedSpecialTokenId).to.equal(848);
+      expect(newCreatedSpecialTokenId).to.equal(882);
     });
         
     it("displays the correct tokenURI", async function () {
